d36c3f0ddd33f1d618f0bc2fa99b5e2d28140f4e
Sean <sean.daxxtrias@gmail.com>
2023-12-16 23:57:06 -0600

    checkpoint rewrite. semi functional

--- src/bot.ts
+++ src/bot.ts
@@ -5,256 +5,109 @@ import { applyTextEffect, Variant } from "./textEffects";
 import axios from 'axios';
 import type { Variant as TextEffectVariant } from "./textEffects";
 
-// Create a bot using the Telegram token
+// Constants and Type Definitions
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
-
-// global app instantiation
 const app = express();
 const PORT = process.env.PORT || 3005;
-app.use(express.json());
+const allEffects: { code: TextEffectVariant; label: string }[] = [
+  { code: 'w', label: 'Monospace' }, { code: 'b', label: 'Bold' },
+  { code: 'i', label: 'Italic' }, { code: 'd', label: 'Doublestruck' },
+  { code: 'o', label: 'Circled' }, { code: 'q', label: 'Squared' }
+];
+const webhookUrl = 'http://localhost:3005/webhook';
+const introductionMessage = `Hello! I'm a Telegram bot.;`;
+const aboutUrlKeyboard = new InlineKeyboard().url("", "");
+const keyboard = new InlineKeyboard();
+
+// Helper Funcs
+const effectCallbackCodeAccessor = (effectCode: TextEffectVariant) => `effect-${effectCode}`;
+const findEffectByLabel = (label: string) => allEffects.find((effect) => effect.label.toLowerCase() === label.toLowerCase())?.code;
+const createInlineKeyboard = (effectCodes: string[]) => {
+  const keyboard = new InlineKeyboard();
+  chunk(effectCodes.map(code => allEffects.find(effect => effect.code === code)), 3)
+  .forEach(chunk => chunk.forEach(effect => effect && keyboard.text(effect.label, effectCallbackCodeAccessor(effect.code)).row()));
+return keyboard;
+};
+const textEffectResponse = (original: string, modified?: string) => `Original: ${original}${modified ? `\nModified: ${modified}` : ''}`;
 
 // Handle the /yo command to greet the user
 bot.command("yo", (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
-
-// Handle the /effect command to apply text effects using an inline keyboard
-type Effect = { code: TextEffectVariant; label: string };
-const allEffects: Effect[] = [
-  {
-    code: "w",
-    label: "Monospace",
-  },
-  {
-    code: "b",
-    label: "Bold",
-  },
-  {
-    code: "i",
-    label: "Italic",
-  },
-  {
-    code: "d",
-    label: "Doublestruck",
-  },
-  {
-    code: "o",
-    label: "Circled",
-  },
-  {
-    code: "q",
-    label: "Squared",
-  },
-];
-
-const effectCallbackCodeAccessor = (effectCode: TextEffectVariant) =>
-  `effect-${effectCode}`;
-
-const effectsKeyboardAccessor = (effectCodes: string[]) => {
-  const effectsAccessor = (effectCodes: string[]) =>
-    effectCodes.map((code) =>
-      allEffects.find((effect) => effect.code === code)
-    );
-  const effects = effectsAccessor(effectCodes);
-
-  const keyboard = new InlineKeyboard();
-  const chunkedEffects = chunk(effects, 3);
-  for (const effectsChunk of chunkedEffects) {
-    for (const effect of effectsChunk) {
-      effect &&
-        keyboard.text(effect.label, effectCallbackCodeAccessor(effect.code));
-    }
-    keyboard.row();
-  }
-
-  return keyboard;
-};
-
-const textEffectResponseAccessor = (
-  originalText: string,
-  modifiedText?: string
-) =>
-  `Original: ${originalText}` +
-  (modifiedText ? `\nModified: ${modifiedText}` : "");
-
-const parseTextEffectResponse = (
-  response: string
-): {
-  originalText: string;
-  modifiedText?: string;
-} => {
-  const originalText = (response.match(/Original: (.*)/) as any)[1];
-  const modifiedTextMatch = response.match(/Modified: (.*)/);
-
-  let modifiedText;
-  if (modifiedTextMatch) modifiedText = modifiedTextMatch[1];
-
-  if (!modifiedTextMatch) return { originalText };
-  else return { originalText, modifiedText };
-};
-
-bot.command("effect", (ctx) =>
-  ctx.reply(textEffectResponseAccessor(ctx.match), {
-    reply_markup: effectsKeyboardAccessor(
-      allEffects.map((effect) => effect.code)
-    ),
-  })
-);
+bot.command('effect', ctx => ctx.reply(textEffectResponse(ctx.match),
+  { reply_markup: createInlineKeyboard(allEffects.map(e => e.code)) }));
+bot.command('start', ctx => ctx.reply(introductionMessage, { reply_markup: aboutUrlKeyboard, parse_mode: 'HTML' }));
+bot.command('webhook', ctx => sendDataToWebhook(ctx.match));
 
 // Handle inline queries
-const queryRegEx = /effect (monospace|bold|italic) (.*)/;
-bot.inlineQuery(queryRegEx, async (ctx) => {
-  const fullQuery = ctx.inlineQuery.query;
-  const fullQueryMatch = fullQuery.match(queryRegEx);
-  if (!fullQueryMatch) return;
+bot.inlineQuery(/effect (monospace|bold|italic) (.*)/, async (ctx) => {
+  if (!Array.isArray(ctx.match)) {
+    console.error('ctx.match is not an array');
+    return;
+  }
 
-  const effectLabel = fullQueryMatch[1];
-  const originalText = fullQueryMatch[2];
+  const [_, label, originalText] = ctx.match;
 
-  const effectCode = allEffects.find(
-    (effect) => effect.label.toLowerCase() === effectLabel.toLowerCase()
-  )?.code;
-  const modifiedText = applyTextEffect(originalText, effectCode as Variant);
+  const effectCode = findEffectByLabel(label);
+  if (!effectCode) {
+    console.error('effect code not found for: ', label);
+    return;
+  }
+  
+  const modifiedText = applyTextEffect(originalText, findEffectByLabel(label) as Variant);
 
-  await ctx.answerInlineQuery(
-    [
-      {
-        type: "article",
-        id: "text-effect",
-        title: "Text Effects",
-        input_message_content: {
-          message_text: `Original: ${originalText}
-            Modified: ${modifiedText}`,
-          parse_mode: "HTML",
-        },
-        reply_markup: new InlineKeyboard().switchInline("Share", fullQuery),
-        url: "http://t.me/EludaDevSmarterBot",
-        description: "Create stylish Unicode text, all within Telegram.",
+  await ctx.answerInlineQuery([
+    {
+      type: 'article',
+      id: 'text-effect',
+      title: 'Text Effects',
+      input_message_content: {
+        message_text: textEffectResponse(originalText, modifiedText),
+        parse_mode: 'HTML'
       },
-    ],
-    { cache_time: 30 * 24 * 3600 } // one month in seconds
-  );
+      reply_markup: new InlineKeyboard().switchInline('Share', `effect ${label} ${originalText}`),
+      url: 'http://t.me/',
+      description: 'Create stylish Unicode text, all within Telegram.'
+    }
+  ], { cache_time: 30 * 24 * 3600 });
 });
 
-// Return empty result list for other queries.
-bot.on("inline_query", (ctx) => ctx.answerInlineQuery([]));
+// func to pass text effect response
+const parseTextEffectResponse = (response: string): { originalText: string, modifiedText?: string } => {
+  const originalTextMatch = response.match(/Original: (.*)/);
+  const modifiedTextWatch = response.match(/Modified: (.*)/);
 
-// Handle text effects from the effect keyboard
-for (const effect of allEffects) {
-  const allEffectCodes = allEffects.map((effect) => effect.code);
+  return {
+    originalText: originalTextMatch ? originalTextMatch[1] : '',
+    modifiedText: modifiedTextWatch ? modifiedTextWatch[1] : undefined
+  };
+};
 
-  bot.callbackQuery(effectCallbackCodeAccessor(effect.code), async (ctx) => {
-    const { originalText } = parseTextEffectResponse(ctx.msg?.text || "");
-    const modifiedText = applyTextEffect(originalText, effect.code);
+// callback handler
+allEffects.forEach(effect => bot.callbackQuery(effectCallbackCodeAccessor(effect.code), async ctx => {
+  keyboard.text(effect.label, `callback_data:${effect.code}`).row();
+  const { originalText } = parseTextEffectResponse(ctx.msg?.text || '');
+  const modifiedText = applyTextEffect(originalText, effect.code);
+  await ctx.editMessageText(textEffectResponse(originalText, modifiedText), 
+  { reply_markup: createInlineKeyboard(allEffects.map(e => e.code).filter(code => code !== effect.code)) });
+}));
 
-    await ctx.editMessageText(
-      textEffectResponseAccessor(originalText, modifiedText),
-      {
-        reply_markup: effectsKeyboardAccessor(
-          allEffectCodes.filter((code) => code !== effect.code)
-        ),
-      }
-    );
-  });
-}
-
-// Handle the /about command
-const aboutUrlKeyboard = new InlineKeyboard().url(
-  "Host your own bot for free.",
-  "https://cyclic.sh/"
-);
-
-// Suggest commands in the menu
-bot.api.setMyCommands([
-  { command: "yo", 
-    description: "Be greeted by the bot"
-  },
-  {
-    command: "effect",
-    description: "Apply text effects on the text. (usage: /effect [text])",
-  },
-  {
-    command: "webhook",
-    description: "Send data to webhook url. (usage: /webhook [args])",
-  },
-]);
-
-// Handle all other messages and the /start command
-const introductionMessage = `Hello! I'm a Telegram bot.
-
-<b>Commands</b>
-/yo - Be greeted by me
-/effect [text] - Show a keyboard to apply text effects to [text]
-/webhook [args] - submit params data to webhook url`;
-
-const replyWithIntro = (ctx: any) =>
-  ctx.reply(introductionMessage, {
-    reply_markup: aboutUrlKeyboard,
-    parse_mode: "HTML",
-  });
-
-bot.command("start", replyWithIntro);
-bot.command("webhook", sendDataToWebhook2);
-bot.on("message", replyWithIntro);
-
-// Start the TG server portion
-if (process.env.NODE_ENV === "production") {
-  // Use Webhooks for the production server
-  const app = express();
+// Route
+if (process.env.NODE_ENV === 'production') {
   app.use(express.json());
-  app.use(webhookCallback(bot, "express"));
-
-  const PORT = process.env.PORT || 3010;
-  app.listen(PORT, () => {
-    console.log(`Bot listening on port ${PORT}`);
-  });
+  app.use(webhookCallback(bot, 'express'));
+  app.listen(PORT, () => console.log(`Bot listening on port ${PORT}`));
 } else {
-  // Use Long Polling for development
   bot.start();
 }
 
-app.post('/webhook', (req: Request, res: Response) => {
-  try {
-    console.log('webhook hit');
-    console.log('data received: ', req.body);
-    res.status(200).send('Data received');
-    
-   } catch (error) {
-      console.error('Error occured: ', error);
-      res.status(500).send('internal server error');
-    }
-  });
-
-// func to actually send the data to the hook
+// Webhook func
 async function sendDataToWebhook(data: string) {
   try {
-    const webhookUrl = 'http://localhost:3005/webhook';
     await axios.post(webhookUrl, {data}, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
+      headers: {  'Content-Type': 'application/json' }
     });
     console.log('data sent to webhook');
-  } catch (error) {
+  }
+  catch (error) {
     console.error('Error occured: ', error);
   }
 }
-
-async function sendDataToWebhook2(ctx: any) {
-  try {
-    const webhookUrl = 'http://localhost:3005/webhook';
-    await axios.post(webhookUrl, {data: ctx.match}, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    console.log('data sent to webhook');
-  } catch (error) {
-    console.error('Error occured: ', error);
-  }
-}
-
-sendDataToWebhook('Test data 12456_!@#$%^&*()_+');
-
-// start the webhook server portion
-app.listen(PORT, () => {
-  console.log(`Bot listening on port ${PORT}`);
-});

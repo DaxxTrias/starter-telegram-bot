323dfb332d2ae7265ebbc6b6c914f944c68a8c14
Sean <sean.daxxtrias@gmail.com>
2023-12-17 01:46:55 -0600

    cleanup, optimizations, bugfixes, redundancy, error checking

--- src/bot.ts
+++ src/bot.ts
@@ -16,10 +16,9 @@ const allEffects: { code: TextEffectVariant; label: string }[] = [
 ];
 const webhookUrl = 'http://localhost:3005/webhook';
 const introductionMessage = `Hello! I'm a Telegram bot.;`;
-const aboutUrlKeyboard = new InlineKeyboard().url("blah", "https://blah.sh/");
-const keyboard = new InlineKeyboard();
+const aboutUrlKeyboard = new InlineKeyboard().url("Website URL", "https://www.google.com/");
 
-// Helper Funcs
+// Helper Functions
 const effectCallbackCodeAccessor = (effectCode: TextEffectVariant) => `effect-${effectCode}`;
 const findEffectByLabel = (label: string) => allEffects.find((effect) => effect.label.toLowerCase() === label.toLowerCase())?.code;
 const createInlineKeyboard = (effectCodes: string[]) => {
@@ -30,15 +29,26 @@ return keyboard;
 };
 const textEffectResponse = (original: string, modified?: string) => `Original: ${original}${modified ? `\nModified: ${modified}` : ''}`;
 
-// Handle the /yo command to greet the user
-bot.command("yo", (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
-bot.command('effect', ctx => ctx.reply(textEffectResponse(ctx.match),
-  { reply_markup: createInlineKeyboard(allEffects.map(e => e.code)) }));
-bot.command('start', ctx => ctx.reply(introductionMessage));
-// bot.on("message", ctx => ctx.reply(introductionMessage, {reply_markup: aboutUrlKeyboard, parse_mode: 'HTML'}));
+// Exception Handlers
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception: ', error);
+  // good spot to perform cleanup of operations / handles
+  // maybe restart automatically?
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  // todo: more extensive logging here
+});
+
+// Bot Command Handlers
+bot.command('yo', (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
+bot.command('effect', ctx => ctx.reply(textEffectResponse(ctx.match), { reply_markup: createInlineKeyboard(allEffects.map(e => e.code)) }));
+bot.command('start', ctx => ctx.reply(introductionMessage, {reply_markup: aboutUrlKeyboard, parse_mode: 'HTML'}));
 bot.command('webhook', ctx => sendDataToWebhook(ctx.match));
 
-// Handle inline queries
+// Inline Query Handler
 bot.inlineQuery(/effect (monospace|bold|italic) (.*)/, async (ctx) => {
   if (!Array.isArray(ctx.match)) {
     console.error('ctx.match is not an array');
@@ -64,14 +74,14 @@ bot.inlineQuery(/effect (monospace|bold|italic) (.*)/, async (ctx) => {
         message_text: textEffectResponse(originalText, modifiedText),
         parse_mode: 'HTML'
       },
-      reply_markup: new InlineKeyboard().switchInline('Share', `effect ${label} ${originalText}`),
+      reply_markup: aboutUrlKeyboard,
       url: 'http://t.me/',
       description: 'Create stylish Unicode text, all within Telegram.'
     }
   ], { cache_time: 30 * 24 * 3600 });
 });
 
-// func to pass text effect response
+// Parse the text effect response
 const parseTextEffectResponse = (response: string): { originalText: string, modifiedText?: string } => {
   const originalTextMatch = response.match(/Original: (.*)/);
   const modifiedTextWatch = response.match(/Modified: (.*)/);
@@ -84,21 +94,21 @@ const parseTextEffectResponse = (response: string): { originalText: string, modi
 
 // callback handler
 allEffects.forEach(effect => bot.callbackQuery(effectCallbackCodeAccessor(effect.code), async ctx => {
-  keyboard.text(effect.label, `callback_data:${effect.code}`).row();
+  // keyboard.text(effect.label, `callback_data:${effect.code}`).row();
   const { originalText } = parseTextEffectResponse(ctx.msg?.text || '');
   const modifiedText = applyTextEffect(originalText, effect.code);
   await ctx.editMessageText(textEffectResponse(originalText, modifiedText), 
   { reply_markup: createInlineKeyboard(allEffects.map(e => e.code).filter(code => code !== effect.code)) });
 }));
 
-// webhook route
+// Webhook Route
 app.post(`/webhook`, async (req: Request, res: Response) => {
   console.log(`webhook triggered: `, req.body);
   res.status(200).send(`data recvd`);
 });
 
-// bot starter logic
-if (process.env.NODE_ENV === 'development') {
+// Bot Server Start Logic
+if (process.env.NODE_ENV === 'production') {
   app.use(express.json());
   app.use(webhookCallback(bot, 'express'));
   app.listen(PORT, () => console.log(`Bot listening on port ${PORT}`));
@@ -106,7 +116,7 @@ if (process.env.NODE_ENV === 'development') {
   bot.start();
 }
 
-// Webhook func
+// Webhook Send Functions
 async function sendDataToWebhook(data: string) {
   try {
     await axios.post(webhookUrl, { data }, { headers: {  'Content-Type': 'application/json' }});
@@ -128,8 +138,3 @@ async function sendDataToWebhook(data: string) {
   }
   }
 }
-
-// start the server portion
-app.listen(PORT, () => {
-  console.log(`Bot listening on port ${PORT}`);
-});

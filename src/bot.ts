4e30bad95a4238af79eca7b0cd741efeab558297
Sean <sean.daxxtrias@gmail.com>
2023-12-18 01:45:12 -0600

    gutting unused portions now that not needed

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,7 @@
 require('dotenv').config();
 import { Bot, InlineKeyboard, webhookCallback } from "grammy";
-import { chunk } from "lodash";
 import express, { Request, Response } from "express";
-import { applyTextEffect, Variant } from "./textEffects";
 import axios from 'axios';
-import type { Variant as TextEffectVariant } from "./textEffects";
 import fs from 'fs';
 import path from 'path';
 
@@ -41,24 +38,8 @@ const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 const PORT = process.env.PORT || 3005;
 const webhookUrl = process.env.WEBHOOK_URL || "http://localhost:3005/webhook";
 const app = express();
-const allEffects: { code: TextEffectVariant; label: string }[] = [
-  { code: 'w', label: 'Monospace' }, { code: 'b', label: 'Bold' },
-  { code: 'i', label: 'Italic' }, { code: 'd', label: 'Doublestruck' },
-  { code: 'o', label: 'Circled' }, { code: 'q', label: 'Squared' }
-];
 const introductionMessage = "Hello! I'm a Telegram bot to help facilitate utilizing Webhooks.";
 const aboutUrlKeyboard = new InlineKeyboard().url("Website URL", "https://www.google.com/");
-
-// Helper Functions
-const effectCallbackCodeAccessor = (effectCode: TextEffectVariant) => `effect-${effectCode}`;
-const findEffectByLabel = (label: string) => allEffects.find((effect) => effect.label.toLowerCase() === label.toLowerCase())?.code;
-const createInlineKeyboard = (effectCodes: string[]) => {
-  const keyboard = new InlineKeyboard();
-  chunk(effectCodes.map(code => allEffects.find(effect => effect.code === code)), 3)
-  .forEach(chunk => chunk.forEach(effect => effect && keyboard.text(effect.label, effectCallbackCodeAccessor(effect.code)).row()));
-return keyboard;
-};
-const textEffectResponse = (original: string, modified?: string) => `Original: ${original}${modified ? `\nModified: ${modified}` : ''}`;
 
 // Exception Handlers
 process.on('uncaughtException', (error) => {
@@ -77,8 +58,6 @@ process.on('unhandledRejection', (reason, promise) => {
 });
 
 // Bot Command Handlers
-bot.command('yo', (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
-bot.command('effect', ctx => ctx.reply(textEffectResponse(ctx.match), { reply_markup: createInlineKeyboard(allEffects.map(e => e.code)) }));
 bot.command('start', ctx => ctx.reply(introductionMessage, {reply_markup: aboutUrlKeyboard, parse_mode: 'HTML'}));
 bot.command('webhook', async ctx => {
   const chatId = ctx.chat.id; // Get the chat ID of the user
@@ -87,59 +66,6 @@ bot.command('webhook', async ctx => {
   // now call the sendDataToWebhook function with the chatId and data
   await sendDataToWebhook(data, chatId);
 });
-
-// Inline Query Handler
-bot.inlineQuery(/effect (monospace|bold|italic) (.*)/, async (ctx) => {
-  if (!Array.isArray(ctx.match)) {
-    console.error('ctx.match is not an array');
-    return;
-  }
-
-  const [_, label, originalText] = ctx.match;
-
-  const effectCode = findEffectByLabel(label);
-  if (!effectCode) {
-    console.error('effect code not found for: ', label);
-    return;
-  }
-  
-  const modifiedText = applyTextEffect(originalText, findEffectByLabel(label) as Variant);
-
-  await ctx.answerInlineQuery([
-    {
-      type: 'article',
-      id: 'text-effect',
-      title: 'Text Effects',
-      input_message_content: {
-        message_text: textEffectResponse(originalText, modifiedText),
-        parse_mode: 'HTML'
-      },
-      reply_markup: aboutUrlKeyboard,
-      url: 'http://t.me/',
-      description: 'Create stylish Unicode text, all within Telegram.'
-    }
-  ], { cache_time: 30 * 24 * 3600 });
-});
-
-// TextEffect Callback Handler
-const parseTextEffectResponse = (response: string): { originalText: string, modifiedText?: string } => {
-  const originalTextMatch = response.match(/Original: (.*)/);
-  const modifiedTextWatch = response.match(/Modified: (.*)/);
-
-  return {
-    originalText: originalTextMatch ? originalTextMatch[1] : '',
-    modifiedText: modifiedTextWatch ? modifiedTextWatch[1] : undefined
-  };
-};
-
-// Callback Handler for special text effects
-allEffects.forEach(effect => bot.callbackQuery(effectCallbackCodeAccessor(effect.code), async ctx => {
-  // keyboard.text(effect.label, `callback_data:${effect.code}`).row();
-  const { originalText } = parseTextEffectResponse(ctx.msg?.text || '');
-  const modifiedText = applyTextEffect(originalText, effect.code);
-  await ctx.editMessageText(textEffectResponse(originalText, modifiedText), 
-  { reply_markup: createInlineKeyboard(allEffects.map(e => e.code).filter(code => code !== effect.code)) });
-}));
 
 // Webhook Routing Function
 app.post(`/webhook`, async (req: Request, res: Response) => {
@@ -169,7 +95,8 @@ async function sendDataToWebhook(data: string, chatId: number) {
     if (axios.isAxiosError(error)) {
       // Check for a 422 status code and send a tailored message
       if (error.response && error.response.status === 422) {
-        await notifyUser(chatId, "The server understood the request but was unable to process the command due to syntax errors or invalid data. Error code 422");
+        await notifyUser(chatId, "The server understood the request but was unable to process the command due to syntax errors or invalid data." +
+        "\nError code 422");
       } else {
         // Handle other errors
         await notifyUser(chatId, `An error occurred while sending data to the webhook.`);
